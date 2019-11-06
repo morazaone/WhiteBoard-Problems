951e71ab5ef8ec63bfac95a74da248fc4ba6e9f9
Juan Moraza <j.moraza@yahoo.com>
2019-11-05 22:47:23 -0600

    Update addTwoNumsFromLinkedList.java

--- addTwoNumsFromLinkedList.java
+++ addTwoNumsFromLinkedList.java
@@ -6,57 +6,58 @@
  *     ListNode(int x) { val = x; }
  * }
  */
-
-public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         
-        boolean addition = false;
-        ListNode hd = new ListNode(0);
-        ListNode curr = hd;
-        ListNode temp = null;
-        
-        int val1 = 0, val2 = 0, total =0;
-        while(l1 != null || l2 != null || addition){
-            
-            if(l1 == null && l2 == null){
-                val1 = 0;
-                val2 = 0;
-            }else if( l1 == null){
-                val1 = 0; 
-                val2 = l2.val; 
-                l2 = l2.next;
-            }else if(l2 == null ){
-                val2 = 0; 
-                val1 = l1.val; 
-                l1 = l1.next;
-            }else{
-                val1 = l1.val;
-                val2 = l2.val;
-                l1 = l1.next;
-                l2 = l2.next;
-            }
-            total = val1 + val2;
-            if(addition)
-                total++;
-                        
-            if(total > 9){
-                total = total%10;
-                addition = true;
-            }else
-                addition = false;
-            
-
-            curr.val = total;
-            
-            if(addition || l1 !=null || l2 != null ){
-            temp = new ListNode(0);
-            curr.next = temp;
-            curr = curr.next;  
-            }
-       
+        if(l1 == null && l2 == null){
+            return null;
         }
-         
-
+        
+        ListNode ans = null;
+        ListNode hd = null;
+        int carry = 0;
+       
+        while(l1 != null || l2 != null){
+            int sum = carry;
+            
+            if(l1 != null){
+                sum += l1.val;
+                l1 = l1.next;
+            }
+            
+            if(l2 != null){
+                sum += l2.val;
+                l2 = l2.next;
+            }
+            
+            if(sum > 9){
+                sum = sum - 10;
+                carry = 1;
+            }else{
+                carry = 0;
+            }
+            
+            
+            ListNode temp = new ListNode(sum);
+            temp.next = null;
+            
+            if(hd == null){
+                hd = temp;
+            }else{
+                ans.next = temp;
+            }
+            
+            ans = temp;
+            
+        }
+        
+        if(carry == 1){
+            ListNode temp = new ListNode(1);
+            temp.next = null;
+            ans.next = temp;
+        }
+        
         return hd;
         
-    
+    }
 }
