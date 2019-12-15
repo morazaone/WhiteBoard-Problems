f70669a90a4963a5f3258765b456c8255f0c3695
Juan Moraza <j.moraza@yahoo.com>
2019-12-15 02:15:17 -0600

    Update longestCommonSubsequence.java

--- longestCommonSubsequence.java
+++ longestCommonSubsequence.java
@@ -1,27 +1,24 @@
 class Solution {
-    public int longestCommonSubsequence(String text1, String text2) {
-        char[] s = text1.toCharArray();
-        char[] t = text2.toCharArray();
-        int[][] dp = new int[s.length+1][t.length+1];
+     public int longestCommonSubsequence(String text1, String text2) {
+        int m = text1.length();
+        int n = text2.length();
         
-        for(int i = 0; i <= s.length; i++) {
-            dp[i][0] = 0;
-        }
-        for(int i = 0; i <= t.length; i++) {
-            dp[0][i] = 0;
-        }
+        char[] t1 = text1.toCharArray();
+        char[] t2 = text2.toCharArray();
         
-        for(int i = 1; i <= s.length; i++) {
-            for(int j = 1; j <= t.length; j++) {
-                if(s[i-1] == t[j-1]) {
-                    dp[i][j] = 1 + dp[i-1][j-1];
-                }
-                else {
-                    dp[i][j] = Math.max(dp[i-1][j],dp[i][j-1]);
+        int[][] dp = new int[m+1][n+1];
+        
+        for(int i=1; i<=m; i++){
+            for(int j=1; j<=n; j++){
+                if(t1[i-1] == t2[j-1]){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else{
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
                 }
             }
         }
         
-        return dp[s.length][t.length];
+        return dp[m][n];
     }
+
 }
